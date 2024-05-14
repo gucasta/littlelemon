2c0f9593710ad2808b28d7c9cc3f687ddd244210
Memo Martell <gucasta@me.com>
2024-05-14 13:28:56 -0600

    User Authentication

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -19,13 +19,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r'tables', views.BookingViewSet)
+router.register(r"tables", views.BookingViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/booking/", include(router.urls)),
     path("restaurant/", include("restaurant.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+    path('api-token-auth/', obtain_auth_token),
 ]
