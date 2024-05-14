c7baf36997123e1587711d94eca9f59462da78c0
Memo Martell <gucasta@me.com>
2024-05-14 14:43:09 -0600

    Final Project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # littlelemon
+cd 'project directory'
+
+pipenv install
+
+pipenv shell
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver
+
+# Repository
+
+https://github.com/gucasta/littlelemon
--- restaurant/tests/tests_views.py
+++ restaurant/tests/tests_views.py
@@ -1,5 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from restaurant.serializers import MenuSerializer
+from restaurant.models import Menu
 
 
 class MenuViewTest(TestCase):
-    
+    def setUp(self):
+        self.create_menu_items()
+        super().setUp()
+
+    def test_list(self):
+        response = self.client.get(reverse("api:menu"))
+        serializer = MenuSerializer(Menu.objects.all(), many=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create(self):
+        data = {"title": "latte", "price": 2.99, "inventory": 5}
+        response = self.client.post(reverse("api:menu"), data=data)
+        serializer = MenuSerializer(Menu.objects.get(title="latte"))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, serializer.data)
